c274203025a473fdc565d8b38739d2c69a40d7b7
Claudiu Vladean <35781140+VladeanClaudiu@users.noreply.github.com>
2025-03-14 10:04:49 +0000

    test init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule, SearchComponent]
 })
 export class AppComponent {
-  title = 'angular-test';
+  title = 'autoaddress-task';
 }
